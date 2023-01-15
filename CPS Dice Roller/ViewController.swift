cf4c1c84f23e9b09b6a827af37acab00e2026f84
Massive <massivepl86@gmail.com>
2023-01-15 03:48:57 +0100

    tested

--- CPS Dice Roller/ViewController.swift
+++ CPS Dice Roller/ViewController.swift
@@ -1,19 +1,60 @@
 //
 //  ViewController.swift
-//  CPS Dice Roller
+//  Dicee-iOS13
 //
-//  Created by Adam Marczyński on 15/01/2023.
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    class Soundmanager {
+        
+        static let instance = Soundmanager()
+        
+        var player: AVAudioPlayer?
+        
+        func playSound() {
+            guard let url = Bundle.main.url(forResource: "diceSound", withExtension: ".mp3") else {return}
+            
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+                player?.play()
+                
+            } catch let error {
+                print(error.localizedDescription)
+            }
+                    
+        }
+        
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+            if motion == .motionShake {
+                rollDice()
+            }
+        }
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+            if motion == .motionShake {
+                Soundmanager.instance.playSound()
+            }
+        }
+    
+    func rollDice() {
+        let diceArray = [ UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix") ]
+       
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+    }
 }
-
